fabc1e04e5cc5619ec625ab45f886bf1246db00f
Mehedi Hassan Jibon <mhj124177@gmail.com>
2022-05-25 16:28:02 +0600

    not found page updated

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
+        <div style={{height:'400px'}}>
             <h1 className='text-center mt-5 text-danger'>Status: 404!!</h1>
             <h2 className='text-center'>Page is not found</h2>
         </div>
